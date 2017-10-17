8ede66fcde919f37ab37d2ca8a4ad03e1b78857f
Dan Blondell <danblondell@gmail.com>
2017-10-17 03:14:02 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -11,6 +11,10 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do | t |
+      t.string :name
+      t.integer :price
+      t.string :image_url
+      t.string :timestamps
     end
   end
 end
